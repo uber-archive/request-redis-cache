645cb0bf732fccad46d4304721fbfcbfdaaffea6
Todd Wolfson <todd@twolfson.com>
2014-04-28 01:10:12 -0700

    Added utility for starting/stopping redis

---
+++ test/utils/redis-server.js
@@ -0,0 +1,21 @@
+// Load in dependencies
+var spawn = require('child_process').spawn;
+
+// Utility method for starting/stopping a redis server
+exports.run = function () {
+  var server;
+  before(function startServer (done) {
+    // Start a server at 9001, callback when it is listening
+    server = spawn('redis-server', ['--port', '9001']);
+    server.stdout.once(function handleServerListen () {
+      done();
+    });
+  });
+  after(function stopServer (done) {
+    // Kill the server and callback when dead
+    server.on('exit', function (code) {
+      done();
+    });
+    server.kill();
+  });
+};
