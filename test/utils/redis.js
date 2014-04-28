593ef283a7212147f86042647fae9aa2fe848db8
Todd Wolfson <todd@twolfson.com>
2014-04-28 01:20:58 -0700

    Consolidating redis utilities for start/stop logic

--- test/utils/redis.js
+++ test/utils/redis.js
@@ -1,13 +1,34 @@
 // Load in dependencies
+var spawn = require('child_process').spawn;
 var redis = require('redis');
 
 // Utility method for create a redis client
-exports.createClient = function () {
-  before(function startServer () {
+exports.run = function () {
+  // Start a server
+  var server;
+  before(function startServer (done) {
+    // Start a server at 9001, callback when it is listening
+    server = spawn('redis-server', ['--port', '9001']);
+    server.stdout.once('data', function handleServerListen (buff) {
+      setTimeout(done, 100);
+    });
+  });
+
+  // Connect a client
+  before(function createClient () {
     this.redis = redis.createClient(9001);
   });
-  after(function stopServer (done) {
+
+  // Teardown client and server
+  after(function exitClient (done) {
     this.redis.quit(done);
     delete this.redis;
+  });
+  after(function stopServer (done) {
+    // Kill the server and callback when dead
+    server.on('exit', function (code) {
+      done();
+    });
+    server.kill();
   });
 };
